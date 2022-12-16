4dcba0e12981c3c4572ed292632337d1f7b50247
SK <sarthakkarandikar03@gmail.com>
2022-12-16 09:06:49 +0000

    Dec 16, 2022, 2:36 PM

--- components/Users.js
+++ components/Users.js
@@ -1,7 +1,9 @@
 const Users = (props) => {
     return (
         <Card className='user-container'>
-            
+            <h1>
+                {props.userN}
+            </h1>
         </Card>
     )
 }
