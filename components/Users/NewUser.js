44b7adbe6d5b514696ba38e9fd9a59e583845655
SK <sarthakkarandikar03@gmail.com>
2022-12-17 10:26:52 +0000

    Dec 17, 2022, 3:56 PM

--- components/Users/NewUser.js
+++ components/Users/NewUser.js
@@ -4,21 +4,21 @@ import './css/NewUser.css'
 const NewUser = (props) => {
     return (
         <div>
-            <Card className='new-user-form-container'></Card>
-            <h1></h1>
-            <form className='new-user-form'>
-                <div className='new-user-forn-label-container'>
-                    <h2 className='new-user-form-label'>
-                        Enter details
-                    </h2>
-                </div>
-                <div className='new-user-form-input-container'>
-                    <input className='new-user-forn-input-name' placeholder='Enter name'>
-                    </input>
-                    <input className='new-user-forn-input-age' placeholder='Enter age'>
-                    </input>
-                </div>
-            </form>
+            <Card className='new-user-form-container'>
+                <form className='new-user-form'>
+                    <div className='new-user-forn-label-container'>
+                        <h2 className='new-user-form-label'>
+                            Enter details
+                        </h2>
+                    </div>
+                    <div className='new-user-form-input-container'>
+                        <input className='new-user-forn-input-name' placeholder='Enter name'>
+                        </input>
+                        <input className='new-user-forn-input-age' placeholder='Enter age'>
+                        </input>
+                    </div>
+                </form>
+            </Card>
         </div>
     )
 }
