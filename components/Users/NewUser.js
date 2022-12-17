52058549871e345e4aa73f3aacc72c5306bda79c
SK <sarthakkarandikar03@gmail.com>
2022-12-17 10:17:10 +0000

    Dec 17, 2022, 3:47 PM

--- components/Users/NewUser.js
+++ components/Users/NewUser.js
@@ -1,7 +1,7 @@
 import Card from '../UI/Card'
 import './css/NewUser.css'
 
-const NewUser = (props) => {
+const NewUser = function (props) => {
     return (
         <Card className='new-user-form-container'>
             <form className='new-user-form'>
