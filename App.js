92e3cc605c161c0226a326702afe7f049f9ce0f2
SK <sarthakkarandikar03@gmail.com>
2022-12-17 11:24:14 +0000

    Dec 17, 2022, 4:54 PM

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ const App = () => {
   return (
     <div className='container'>
       <Card className='new-user-container'>
-        <NewUser >
+        <NewUser onAddUser = {new}>
         </NewUser>
       </Card>
       <User user={users[0]}>
