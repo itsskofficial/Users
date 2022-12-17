8acf9640facafd8c515866eb542fd3cb1cf895b8
SK <sarthakkarandikar03@gmail.com>
2022-12-17 10:21:10 +0000

    Dec 17, 2022, 3:51 PM

--- components/UI/Card.js
+++ components/UI/Card.js
@@ -2,7 +2,7 @@ import './css/Card.css'
 
 const Card = (props) => {
     return (
-        <div className={'card' + {props.className}}>
+        <div className={'card ' + props.className}>
         </div>
     )
 }
