6d694d65b319be20918ffc6667661ed17cc5c6fb
SK <sarthakkarandikar03@gmail.com>
2022-12-17 10:25:41 +0000

    Dec 17, 2022, 3:55 PM

--- components/Users/NewUser.js
+++ components/Users/NewUser.js
@@ -3,7 +3,7 @@ import './css/NewUser.css'
 
 const NewUser = (props) => {
     return (
-       
+        <div></div>
             <h1></h1>
             <form className='new-user-form'>
                 <div className='new-user-forn-label-container'>
