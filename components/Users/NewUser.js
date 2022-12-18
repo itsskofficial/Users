92d421c3d140fc75e37d076062885148017af864
SK <sarthakkarandikar03@gmail.com>
2022-12-18 08:53:20 +0000

    Dec 18, 2022, 2:23 PM

--- components/Users/NewUser.js
+++ components/Users/NewUser.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './css/NewUser.css'
 
 const NewUser = (props) => {
+    
     const [userName, setUserName] = useState('')
     const [userAge, setUserAge] = useState()
     const [newUserForm, setNewUserForm] = useState(
