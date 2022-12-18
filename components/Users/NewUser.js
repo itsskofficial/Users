614f4f7238f1ba74de888052b412415d4e0ddfa5
SK <sarthakkarandikar03@gmail.com>
2022-12-18 08:54:54 +0000

    Dec 18, 2022, 2:24 PM

--- components/Users/NewUser.js
+++ components/Users/NewUser.js
@@ -25,35 +25,6 @@ const NewUser = (props) => {
                 </form>
     )
 
-    const formSubmitHandler = (event) => {
-        event.preventDefault()
-        props.onAddUser({
-            name: userName,
-            age:userAge
-        })
-
-        setNewUserForm(
-            <form className='new-user-form' onSubmit={formSubmitHandler}>
-                    <div className='new-user-forn-label-container'>
-                        <h2 className='new-user-form-label'>
-                            Enter details
-                        </h2>
-                    </div>
-                    <div className='new-user-form-input-container'>
-                        <input className='new-user-form-input-name' placeholder='Enter name' onChange={nameChangeHandler}>
-                        </input>
-                        <input className='new-user-form-input-age' placeholder='Enter age' onChange={ageChangeHandler}>
-                        </input>
-                    </div>
-                    <div className = 'new-user-form-actions'>
-                        <button className='new-user-form-button' type='submit'>
-                            Add User
-                        </button>
-                    </div>
-                </form>
-        )
-    }
-
     const nameChangeHandler = (event) => {
         setUserName(event.target.value)
     }
