d239281dd276fc2a6705cc263594570693f9a039
SK <sarthakkarandikar03@gmail.com>
2022-12-18 10:11:17 +0000

    Dec 18, 2022, 3:41 PM

--- components/Users/NewUser.js
+++ components/Users/NewUser.js
@@ -24,26 +24,6 @@ const NewUser = (props) => {
             age:userAge
         })
 
-        setNewUserForm(
-            <form className='new-user-form' onSubmit={formSubmitHandler}>
-                    <div className='new-user-forn-label-container'>
-                        <h2 className='new-user-form-label'>
-                            Enter details
-                        </h2>
-                    </div>
-                    <div className='new-user-form-input-container'>
-                        <input type='text' className='new-user-form-input-name' placeholder='Enter name' onChange={nameChangeHandler} value={userName}>
-                        </input>
-                        <input type= 'number' className='new-user-form-input-age' onChange={ageChangeHandler} value={userAge}>
-                        </input>
-                    </div>
-                    <div className = 'new-user-form-actions'>
-                        <Button type='submit' onClick={formSubmitHandler}>
-                            Add User
-                        </Button>
-                    </div>
-                </form>
-        )
     }
 
     
