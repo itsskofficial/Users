9bdb534c62d34b1509b325007611dac3c9bdcce3
SK <sarthakkarandikar03@gmail.com>
2022-12-18 09:56:40 +0000

    Dec 18, 2022, 3:26 PM

--- components/UI/ErrorModal.js
+++ components/UI/ErrorModal.js
@@ -4,7 +4,9 @@ import Card from './Card'
 const ErrorModal = (props) => {
     return (
         <Card>
-            
+            <header>
+                <h2>
+                
         </Card>
     )
 }
