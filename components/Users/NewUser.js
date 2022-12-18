13892e760c30a0db4a256e8f80ed547dc027e8d8
SK <sarthakkarandikar03@gmail.com>
2022-12-18 08:55:06 +0000

    Dec 18, 2022, 2:25 PM

--- components/Users/NewUser.js
+++ components/Users/NewUser.js
@@ -4,6 +4,36 @@ import './css/NewUser.css'
 const NewUser = (props) => {
     const [userName, setUserName] = useState('')
     const [userAge, setUserAge] = useState()
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault()
+        props.onAddUser({
+            name: userName,
+            age:userAge
+        })
+
+        setNewUserForm(
+            <form className='new-user-form' onSubmit={formSubmitHandler}>
+                    <div className='new-user-forn-label-container'>
+                        <h2 className='new-user-form-label'>
+                            Enter details
+                        </h2>
+                    </div>
+                    <div className='new-user-form-input-container'>
+                        <input className='new-user-form-input-name' placeholder='Enter name' onChange={nameChangeHandler}>
+                        </input>
+                        <input className='new-user-form-input-age' placeholder='Enter age' onChange={ageChangeHandler}>
+                        </input>
+                    </div>
+                    <div className = 'new-user-form-actions'>
+                        <button className='new-user-form-button' type='submit'>
+                            Add User
+                        </button>
+                    </div>
+                </form>
+        )
+    }
+    
     const [newUserForm, setNewUserForm] = useState(
         <form className='new-user-form' onSubmit={formSubmitHandler}>
                     <div className='new-user-forn-label-container'>
