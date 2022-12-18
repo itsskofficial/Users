e1fba32e473b33fc4d1c534b475b37a26c2833ef
SK <sarthakkarandikar03@gmail.com>
2022-12-18 09:28:14 +0000

    Dec 18, 2022, 2:58 PM

--- components/Users/NewUser.js
+++ components/Users/NewUser.js
@@ -33,7 +33,7 @@ const NewUser = (props) => {
                     <div className='new-user-form-input-container'>
                         <input className='new-user-form-input-name' placeholder='Enter name' onChange={nameChangeHandler} value={userName}>
                         </input>
-                        <input className='new-user-form-input-age' placeholder='Enter age' value={userAge} onChange={ageChangeHandler} >
+                        <input className='new-user-form-input-age' placeholder='Enter age' onChange={ageChangeHandler} value={userAge}>
                         </input>
                     </div>
                     <div className = 'new-user-form-actions'>
