e1d28dea0a3af5383b37c784908da829537e965b
SK <sarthakkarandikar03@gmail.com>
2022-12-18 09:54:09 +0000

    Dec 18, 2022, 3:24 PM

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -3,6 +3,7 @@ import './css/Button.css'
 const Button = (props) => {
     return (
         <button onClick={props.onClick} type={props.type} className='button'>
+            {props.children}
         </button>
     )
 }
