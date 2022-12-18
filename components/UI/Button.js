219fac03aefe9c60fdbe0a9a48a9f9aefa0fa665
SK <sarthakkarandikar03@gmail.com>
2022-12-18 09:51:29 +0000

    Dec 18, 2022, 3:21 PM

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -1,6 +1,6 @@
 const Button = (props) => {
     return (
-        <button onClick={ props.onClick } className=''>
+        <button onClick={ props.onClick } className='button'>
         </button>
     )
 }
