1d1490b67e3d89097f1ef3c0a7aa87a2a69ff9af
SK <sarthakkarandikar03@gmail.com>
2022-12-18 10:11:50 +0000

    Dec 18, 2022, 3:41 PM

--- components/Users/NewUser.js
+++ components/Users/NewUser.js
@@ -26,31 +26,6 @@ const NewUser = (props) => {
 
     }
 
-    
-
-
-    const [newUserForm, setNewUserForm] = useState(
-        <form className='new-user-form' onSubmit={formSubmitHandler}>
-                    <div className='new-user-forn-label-container'>
-                        <h2 className='new-user-form-label'>
-                            Enter details
-                        </h2>
-                    </div>
-                    <div className='new-user-form-input-container'>
-                        <input type='text' className='new-user-form-input-name' placeholder='Enter name' onChange={nameChangeHandler} value={userName}>
-                        </input>
-                        <input type='number' className='new-user-form-input-age' placeholder='Enter age' onChange={ageChangeHandler} value={userAge}>
-                        </input>
-                    </div>
-                    <div className = 'new-user-form-actions'>
-                        <button className='new-user-form-button' type='submit'>
-                            Add User
-                        </button>
-                    </div>
-                </form>
-    )
-
-
     return (
         <div>
             <form className='new-user-form' onSubmit={formSubmitHandler}>
