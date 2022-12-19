79b4e0779b26d055dd277edd68fb9c2fb4349352
SK <sarthakkarandikar03@gmail.com>
2022-12-19 11:27:13 +0000

    Dec 19, 2022, 4:57 PM

--- components/UI/ErrorModal.js
+++ components/UI/ErrorModal.js
@@ -37,7 +37,7 @@ const ErrorModal = (props) => {
     return (
         <Fragment>
             {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}/>,document.getElementById('backdrop-root'))}
-            {ReactDOM.createPortal(<)}
+            {ReactDOM.createPortal(<ModalOverlay )}
         </Fragment>
         
     )
